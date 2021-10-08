0caac2d7900ccad2de500e9ecb368d3b74f038b0
Hans Victor Munck-Westh <durandal2nd@gmail.com>
2021-10-08 12:06:09 +0200

    first commit

---
+++ src/main/java/com/herokuapi001/HerokuApi001Application.java
@@ -0,0 +1,13 @@
+package com.herokuapi001;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HerokuApi001Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HerokuApi001Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/herokuapi001/HerokuApi001ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.herokuapi001;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HerokuApi001ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
